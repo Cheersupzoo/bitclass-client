1ebbec7d3da053bce2bc80e89d13e5641cdbaa91
SoponwichT <aot_aln32@hotmail.com>
2021-05-11 16:24:40 +0700

    add more about text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 export default function About() {
-    return <div className="container pt-2">
-    <h2>About</h2>
-  </div>;
+    return <div className="container pt-5">
+    <h2 className="mt-5">About</h2>
+    <h5>BitClass™ is an web application that allows teacher and student to host a class with feature such as posting feed and upload file while all the in-class data is keep between client and no private data store on the server. By using Peer-to-Peer, all the data will trasfer directly to each client so no worry that we can collect your data.</h5>
+    <h6 className="mt-5"><span style={{color:"grey",fontWeight:"400"}}> This website is use for CSS334 Computer Networks and Internetworking subject.</span></h6>
+    </div>;
   }
